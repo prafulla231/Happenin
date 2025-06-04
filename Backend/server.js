45f280f4b8bbaf66c5c99d3751151aa0ec62bede
prafulla231 <prafullabadgujar2512@gmail.com>
2025-06-04 12:49:38 +0530

    Basic Structure

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+await connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
